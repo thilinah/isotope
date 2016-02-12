4cc9f8c280e502001133eb4a8841a505764f63c0
Thilina Hasantha <thilina.hasantha@gmail.com>
2016-02-13 00:55:47 +0530

    Adding new theme

--- compile.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-rm -r /root/dev/jekyll/icehrmblog2/_site
-mkdir /root/dev/jekyll/icehrmblog2/_site
-rm -r /var/www/icehrmlive.com
-mkdir /var/www/icehrmlive.com
-jekyll build
-cp -r /root/dev/jekyll/icehrmblog2/_site/* /var/www/icehrmlive.com/
